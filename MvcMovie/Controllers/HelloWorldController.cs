01b217d7b65957a878d1a7f9532cd7c952dcd2b7
Mapendo Kelly <make48949@edu.ucl.dk>
2023-09-22 15:48:04 +0200

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+       // An action method that calls
+       // the controller's View method
+       // To generate an HTML response by using
+       // a view template.
+       public IActionResult Index() 
+        {
+            return View();
+        }
+
+        //
+        // Olde "Welcome" Method = GET: /HelloWorld/Welcome/
+        // Requires using System.Text.Encodings.Web
+
+        // New action "Welcome" Method
+        // That returns the controller's View method
+        // And use the "ViewData" dictionary object
+        // That contains data that will be passed to the view. 
+
+        public IActionResult Welcome(string name, int numTimes = 11) 
+        {
+            ViewData["Message"] = "Hello" + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
