062f6746fcead9edd71768ce32476cb7cfb2a357
Mapendo Kelly <make48949@edu.ucl.dk>
2023-10-22 21:05:02 +0200

    Added MovieGenreModel class + a few.

--- MvcMovie/Migrations/20231017153208_InitialCreate.Designer.cs
+++ MvcMovie/Migrations/20231017153208_InitialCreate.Designer.cs
@@ -7,8 +7,6 @@ using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MvcMovie.Data;
 
-#nullable disable
-
 namespace MvcMovie.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
@@ -18,7 +16,7 @@ namespace MvcMovie.Migrations
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
-#pragma warning disable 612, 618
+
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.12")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
@@ -49,7 +47,7 @@ namespace MvcMovie.Migrations
 
                     b.ToTable("Movie");
                 });
-#pragma warning restore 612, 618
+
         }
     }
 }
--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models
 {
@@ -7,11 +8,17 @@ namespace MvcMovie.Models
         public int Id { get; set; }
         public string? Title { get; set; }
 
+        // The display attribute that should specify what to display
         //The DataType attribute on ReleaseDate 
         //that makes sure that the date is displayed and not the time information. 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string? Genre { get; set; }
+
+        // A required attribute so that Entity Framework
+        // can correctly locate Price to currency in the database.
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
 
     }
